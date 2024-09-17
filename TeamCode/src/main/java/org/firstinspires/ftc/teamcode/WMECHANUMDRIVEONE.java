77fb664538c8d4f7e27b8790eb4b5bbee3c82d98
wildfire11 <90204764+wildfire11@users.noreply.github.com>
2024-09-17 12:07:45 -0400

    Added mechanum drive starter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WMECHANUMDRIVEONE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WMECHANUMDRIVEONE.java
@@ -56,8 +56,7 @@ public class WMECHANUMDRIVEONE extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -69,14 +68,11 @@ public class WMECHANUMDRIVEONE extends OpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -100,21 +96,33 @@ public class WMECHANUMDRIVEONE extends OpMode
     /*
      * Code to run REPEATEDLY after the driver hits START but before they hit STOP
      */
+    var frontLeftMotor;
     @Override
     public void loop() {
         // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
+        double drive = -gamepad1.left_stick_y;
+        double turn  =  gamepad1.right_stick_x;
+        frontLeftMotor    = Range.clip(drive + turn, -1.0, 1.0) ;
+        frontRightMotor   = Range.clip(drive - turn, -1.0, 1.0) ;
+        backRightMotor    = Range.clip(doublenumber drive + turn min:-1.0, max:1.0);
+        backLeftMotor     = Range.clip(doublenumber drive + turn, min:-1.0 max:1.0);
+
+        double drive = gamepad1.a;
+        frontRightMotor.setpower=1;
+        frontLeftMotor.setpower=-1;
+        backLeftMotor.setpower=1;
+        backRightMotor.setpower=-1;
         // Choose to drive using either Tank Mode, or POV Mode
         // Comment out the method that's not used.  The default below is POV.
 
         // POV Mode uses left stick to go forward, and right stick to turn.
         // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
 
         // Tank Mode uses one stick to control each wheel.
         // - This requires no math, but it is hard to drive forward slowly and keep straight.
@@ -122,9 +130,6 @@ public class WMECHANUMDRIVEONE extends OpMode
         // rightPower = -gamepad1.right_stick_y ;
 
         // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
-
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
