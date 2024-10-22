e2f6fc9ce67c1d5d07fa305fb437079886ee5869
wildfire11 <90204764+wildfire11@users.noreply.github.com>
2024-10-22 16:35:03 -0400

    e2qeq

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton.java
@@ -1,24 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import androidx.annotation.NonNull;
-
-// RR-specific imports
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-
-// Non-RR imports
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-
 
 public class auton {
     import androidx.annotation.NonNull;
@@ -30,15 +11,15 @@ public class auton {
     import com.acmerobotics.roadrunner.Pose2d;
     import com.acmerobotics.roadrunner.SequentialAction;
     import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-
+    import com.acmerobotics.roadrunner.ftc.Actions;
+    import com.acmerobotics.roadrunner.ParallelAction;
 // Non-RR imports
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
+    import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+    import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+    import com.qualcomm.robotcore.hardware.HardwareMap;
+    import com.qualcomm.robotcore.hardware.Servo;
+    import com.qualcomm.robotcore.hardware.DcMotorEx;
+    import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 
 }
