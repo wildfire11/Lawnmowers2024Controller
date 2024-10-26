9020c79d9a962691663554783f3c0d2578053dba
nathanbengtson <nathanbengtson48@gmail.com>
2024-10-26 16:01:54 -0400

    Servo fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -4,18 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Servo Test IKEA", group = "MAIN")
+@TeleOp(name = "Servo Test", group = "MAIN")
 public class ServoTest extends LinearOpMode {
     Servo servo1;
     @Override
     public void runOpMode() throws InterruptedException {
         servo1=hardwareMap.get(Servo.class,"servo");
         waitForStart();
+        servo1.getConnectionInfo();
         while(opModeIsActive()) {
             if (gamepad1.y) {
                 telemetry.addLine("Y is pressed");
                 telemetry.update();
-                servo1.setPosition(1);
+                servo1.setPosition(0.5);
             }else{
                 servo1.setPosition(0);
                 telemetry.addLine("Y is not pressed");
